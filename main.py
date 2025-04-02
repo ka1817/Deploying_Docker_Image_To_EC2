f13b72abe81541998272411dcc8b3f8dbfc295c5
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-04-02 15:44:45 +0530

    New_Updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app=FastAPI(title="Basic Project to deploy docker image EC2 using github actions
 
 @app.get("/")
 def home():
-    return "Welcome to the world of EC2 Deployment with docker" 
+    return "Welcome to the world of EC-2" 
 
 if __name__=="__main__":
     uvicorn.run(app,host='0.0.0.0',port=8000)
