f84185c2b1d0a3d1fea8e8ca25a31ee4d61fe7d3
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-04-02 11:56:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import uvicorn
+app=FastAPI(title="Basic Project to deploy docker image EC2 using github actions")
+
+@app.get("/")
+def home():
+    return "Hello World" 
+
+if __name__=="__main__":
+    uvicorn.run(app,host='0.0.0.0',port=2000)
